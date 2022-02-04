32b9c7427db68e09b9a4037ce6ea102046e2a5b7
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2022-02-04 05:43:42 -0300

    Add files via upload

---
+++ GTAVPedDB/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GTAVPedDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string foto = "https://docs.fivem.net/peds/" + comboBox1.Text + ".png";
+            pictureBox1.ImageLocation = foto;
+
+ 
+        }
+    }
+}
